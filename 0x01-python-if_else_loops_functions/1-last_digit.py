63d553c5473e7a5cc29c4a328964dce7b44aa30b
Thomas Graeff <676@holbertonschool.com>
2019-05-01 05:24:43 +0000

    Fixed trailing whitespace

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 
 if number > 0:
-    rem = number % 10 
+    rem = number % 10
 else:
     rem = abs(number) % 10 * -1
-
-print("Last digit of {} is {} and is".format(number, rem), end="")
-
+print("Last digit of {} is {} and is".format(number, rem), end=" ")
 if rem > 5:
-    print(" greater than 5")
+    print("greater than 5")
 elif rem == 0:
-    print(" 0")
+    print("0")
 else:
-    print(" less than 6 and not 0")
+    print("less than 6 and not 0")

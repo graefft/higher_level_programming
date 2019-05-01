6dc9106a568fe6af42c11ffc567f3eff406f106a
Thomas Graeff <676@holbertonschool.com>
2019-05-01 04:13:38 +0000

    Fixed indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10000, 10000)
 
-if number > 0
+if number > 0:
     rem = number % 10 
-else 
+else:
     rem = abs(number) % 10 * -1
 
 print("Last digit of {} is {} and is".format(number, rem), end="")

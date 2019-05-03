f9352e2e9675839f68ff9c95629b0eb2c79b1c4f
Thomas Graeff <676@holbertonschool.com>
2019-05-03 01:53:16 +0000

    Add 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     if length == 0:
         print('0 arguments.')
     elif length == 1:
-        print('1 argument')
+        print('1 argument:')
         print('1: {:s}'.format(sys.argv[1]))
     else:
-        print("{:d} arguments".format(length))
+        print("{:d} arguments:".format(length))
         for i in range(length):
             print('{:d}: {:s}'.format(i + 1, sys.argv[i + 1]))

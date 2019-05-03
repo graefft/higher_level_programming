5968a9fd11dcc645bda8d1778537e032b2286448
Thomas Graeff <676@holbertonschool.com>
2019-05-03 04:15:16 +0000

    Added underscore in my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     from sys import argv
     from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
-        print('Usage: ./100-mycalculator.py <a> <operator> <b>')
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
         a = int(argv[1])
@@ -17,5 +17,5 @@ if __name__ == '__main__':
         elif argv[2] == "/":
             print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
         else:
-            print('Unknown operator. Available operators: +, -, * and /')
+            print("Unknown operator. Available operators: +, -, * and /")
             exit(1)

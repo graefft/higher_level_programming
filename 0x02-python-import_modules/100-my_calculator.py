afa8a6516973db162ba8e8010b410e23ca89033f
Thomas Graeff <676@holbertonschool.com>
2019-05-03 03:31:39 +0000

    Added int cast to result

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    from calculator_1 import add, sub, mul, div
     from sys import argv
+    from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
         print('Usage: ./100-mycalculator.py <a> <operator> <b>')
         exit(1)
@@ -9,13 +9,13 @@ if __name__ == '__main__':
         a = int(argv[1])
         b = int(argv[3])
         if argv[2] == "+":
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+            print('{:d} + {:d} = {:d}'.format(a, b, add(int(a, b))))
         elif argv[2] == "-":
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(int(a, b))))
         elif argv[2] == "*":
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(int(a, b))))
         elif argv[2] == "/":
-            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+            print('{:d} / {:d} = {:d}'.format(a, b, div(int(a, b))))
         else:
             print('Unknown operator. Available operators: +, -, * and /')
             exit(1)

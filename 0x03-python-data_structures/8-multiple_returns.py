ae336be9912b40617d48c799b98dfff157395007
Thomas Graeff <676@holbertonschool.com>
2019-05-09 06:16:20 +0000

    Fix: add case for if length is 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     first = ()
-    if sentence is None:
+    if sentence is None or len(sentence) == 0:
         first = (0, None)
     else:
         first = (len(sentence), sentence[0])

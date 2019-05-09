736132e99479f6fdec837a701c1cc108cc75cd30
Thomas Graeff <676@holbertonschool.com>
2019-05-09 06:22:40 +0000

    Fix: indentation error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if len(my_list) < 1:
         return None
     max = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > max:
-            max = my_list[i]
-        return max
+    for i in my_list:
+        if i > max:
+            max = i
+    return max

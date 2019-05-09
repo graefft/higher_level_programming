d6f02679ea516829e709c11e74773411841731e7
Thomas Graeff <676@holbertonschool.com>
2019-05-09 06:10:01 +0000

    Fix: change else statement

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,6 @@ def multiple_returns(sentence):
     if sentence is None:
         first = None
     else:
-        first = sentence[0]
+        first = len(sentence), sentence[0]
 
-    tup_ret = (len(sentence), first)
-    return tup_ret
+    return first

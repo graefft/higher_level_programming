614c126a2c074b14e4e26266f1ace1676b60adac
Thomas Graeff <676@holbertonschool.com>
2019-05-09 05:47:06 +0000

    Debug 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
+    our_string = ""
     for i in my_string:
-        if i != "c" or i != "C":
-            new += i
-    return new
+        if (i != "c" and i != "C"):
+            our_string += i
+    return our_string

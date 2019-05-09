c1047eabd4c534a2318f2c5cdbf2029c42b396b9
Thomas Graeff <676@holbertonschool.com>
2019-05-09 06:12:23 +0000

    Fix: added length parameter if sentence is none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence is None:
-        first = None
+        first = (0, None)
     else:
-        first = len(sentence), sentence[0]
-
+        first = (len(sentence), sentence[0])
     return first

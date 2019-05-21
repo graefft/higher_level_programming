4beec3279e961b36f2db2e147d544bc3ca27c2be
Thomas Graeff <676@holbertonschool.com>
2019-05-21 05:24:55 +0000

    Add Break statement

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,10 +4,15 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if (i > a):
-                raise Exception:
-                    'Too far'
+                raise Exception('Too far')
             else:
-                result += (a ** b / i)
-            finally:
-                result = (a + b)
-        return (result)
+                result += (a ** b) / i
+        except:
+                result = (b + a)
+                break
+    return (result)
+
+import dis
+
+
+print(dis.dis(magic_calculation))

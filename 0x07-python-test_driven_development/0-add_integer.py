3bb65234cec2a7835777b27be98f8bf543c77c73
Thomas Graeff <676@holbertonschool.com>
2019-05-25 07:34:48 +0000

    Changed casting for floats

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,8 +4,12 @@
 
 def add_integer(a, b=98):
     """Return sum of a and b"""
-    if type(a) is not int and type(a) is not float:
+    if type(a) is float:
+        a = int(a)
+    elif type(a) is not int:
         raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
+    if type(b) is float:
+        b = int(b)
+    if type(b) is not int:
         raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    return (a + b)

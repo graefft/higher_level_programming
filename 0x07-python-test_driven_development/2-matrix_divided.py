ebd4d5c009461a9ce9a8e21ac16d551d143f282f
Thomas Graeff <676@holbertonschool.com>
2019-05-25 16:10:49 +0000

    Fixed indentation of return block

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,10 +8,14 @@ def matrix_divided(matrix, div):
         for j in i:
             if type(j) is not int and type(j) is not float:
                 raise TypeError(
-                    'matrix must be a matrix (list of lists)' +
+                    'matrix must be a matrix (list of lists) ' +
                     'of integers/floats')
 
-        if len(matrix[0]) is not len(matrix[1]):
+        if type(i) is not list:
+            raise TypeError(
+                    'matrix must be a matrix (list of lists) ' +
+                    'of integers/floats')
+        if len(i) is not len(matrix[0]):
             raise TypeError('Each row of the matrix must have the same size')
 
         if type(div) is not int and type(div) is not float:
@@ -22,9 +26,9 @@ def matrix_divided(matrix, div):
 
         matrix_div = []
 
-        for i in matrix:
-            row = []
-            for j in i:
-                row.append(round(j / div, 2))
-            matrix_div.append(row)
-        return (matrix_div)
+    for i in matrix:
+        row = []
+        for j in i:
+            row.append(round(j / div, 2))
+        matrix_div.append(row)
+    return (matrix_div)

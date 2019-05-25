38f40ab2d4287a394ff9fbdd2ca3976127be8d4d
Thomas Graeff <676@holbertonschool.com>
2019-05-25 08:43:25 +0000

    Changed parentheses

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,11 +11,11 @@ class TestMaxInteger(unittest.TestCase):
 
         def test_normal(self):
             """Test normal case"""
-            self.assertEqual(max_integer([1, 2, 3, 4], 4))
+            self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
         def test_one(self):
             """Test one int"""
-            self.assertEqual(max_integer([3], 1))
+            self.assertEqual(max_integer([3]), 3)
 
         def test_empty(self):
             """Test empty list"""
@@ -23,23 +23,24 @@ class TestMaxInteger(unittest.TestCase):
 
         def test_same(self):
             """Test same ints"""
-            self.assertEqual(max_integer([21, 21, 21, 21], 4))
+            self.assertEqual(max_integer([21, 21, 21, 21]), 21)
 
         def test_string(self):
             """Test with string"""
-            self.assertEqual(max_integer(["Hi", 2, 3, 4], 4))
+            mix = ["Hi", 2, 3, 4]
+            self.assertRaises(TypeError, max_integer, mix)
 
         def test_float(self):
             """Test with float"""
-            self.assertEqual(max_integer([1, 2, 3, 4.4], 4))
+            self.assertEqual(max_integer([1, 2, 3, 4.4]), 4.4)
 
         def test_negative(self):
             """Test negative int"""
-            self.assertEqual(max_integer([-1, -2, -3, -4]. 4))
+            self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
         def test_large(self):
             """Test large numbers"""
-            self.assertEqual(max_integer([99999, 999999, 99999999], 9999999))
+            self.assertEqual(max_integer([99999, 999999, 99999999]), 99999999)
 
 
 if __name__ == '__main__':

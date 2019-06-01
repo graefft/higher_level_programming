ddd1111acdf79652fc308ff875a36b48f63acfaa
Thomas Graeff <676@holbertonschool.com>
2019-06-01 16:51:06 +0000

    Add 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,34 +10,48 @@
 
 int is_palindrome(listint_t **head)
 {
-	int length = 0;
-	int i = 0, j = 0;
-	listint_t *temp;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *current = *head;
+	listint_t *reverse = NULL;
 
-	if (!head)
-		return (0);
-	if (*head == NULL)
-		return (1);
-	temp = *head;
-	while (temp)
+	if (head)
 	{
-		temp = temp->next;
-		length++;
-	}
-	array = malloc(sizeof(int) * length);
-	if (!array)
-		return (0);
-	temp = *head;
-	while (temp)
-	{
-		temp = temp->next;
-		i++;
-		array[i] = temp->n;
-	}
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		if (array[i] != array [j])
-			return (0);
+		while (fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		reverse = reverse_list(&slow);
+
+		while (reverse)
+		{
+			if (current->n != reverse->n)
+				return (0);
+			reverse = reverse->next;
+			current = current->next;
+		}
 	}
 	return (1);
+}
+
+/**
+ *  reverse - reverses linked list
+ *  @head: double pointer to address of head
+ *  Return: pointer to first node of reversed list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+	listint_t *reverse = NULL;
+	listint_t *prev = *head;
+	listint_t *next = *head;
+
+	while (prev)
+	{
+		next = next->next;
+		prev->next = reverse;
+		reverse = prev;
+		prev = next;
+	}
+	return (reverse);
 }

cfbd910c2c2ee493645e0654a56edd7ccf07ebf2
Thomas Graeff <676@holbertonschool.com>
2019-06-01 06:33:57 +0000

    Switched to matmul

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,5 +7,5 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Function to multiply 2 matrices using NumPy"""
-    result = np.dot(m_a, m_b)
+    result = np.matmul(m_a, m_b)
     return (result)

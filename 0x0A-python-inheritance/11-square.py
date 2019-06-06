15db210cc2e1da016e6f76a2fcb6e0b9bb86f8f8
Thomas Graeff <676@holbertonschool.com>
2019-06-06 07:22:57 +0000

    Fix: change order

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,9 +9,9 @@ class Square(Rectangle):
     """Class Square that inherits from 'Rectangle'"""
     def __init__(self, size):
         """Instantiation with private size attribute"""
-        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def __str__(self):
         """Prints Square attributes"""

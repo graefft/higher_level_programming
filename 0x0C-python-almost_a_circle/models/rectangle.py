f28a43982ff97f83862b8f43d0a3b710d3716540
Thomas Graeff <676@holbertonschool.com>
2019-06-12 22:58:24 +0000

    Adds Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,6 +2,9 @@
 """This module creates class Rectangle"""
 
 
+from models.base import Base
+
+
 class Rectangle(Base):
     """class Rectangle that inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):

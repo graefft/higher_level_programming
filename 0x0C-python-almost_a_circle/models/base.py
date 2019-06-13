c91478852345ad9c7ed88ef6394e2e785cf8663b
Thomas Graeff <676@holbertonschool.com>
2019-06-13 02:29:02 +0000

    Adds JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,10 +26,18 @@ class Base():
             return (json.dumps(list_dictionaries))
 
     @staticmethod
-    def from_json_string(json_string):    
+    def from_json_string(json_string):
         """returns list of JSON string representation"""
         if json_string is None:
             return ([])
         else:
             return (json.loads(json_string))
 
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes JSON string representation of list_objs to a file"""
+        list_dict = []
+        for i in list_objs:
+            list_dict += [i.to_dictionary()]
+        with open(cls.__name__ + '.json', 'w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(list_dict))

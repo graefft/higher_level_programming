5c7cc1c4cbc6a8a40a97b7cafcf3d4d82ab27dc0
Thomas Graeff <676@holbertonschool.com>
2019-06-13 02:00:20 +0000

    Fix: adds empty string case

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,4 +20,7 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON string representation of list_dictionaries"""
-        return (json.dumps(list_dictionaries))
+        if list_dictionaries is None or len(list_dictionaries) <= 0:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))

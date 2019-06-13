c414f740d6312e2ca400f7b2c8a0760a114675a2
Thomas Graeff <676@holbertonschool.com>
2019-06-13 02:19:56 +0000

    Adds from_json_string()

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,12 @@ class Base():
             return ("[]")
         else:
             return (json.dumps(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):    
+        """returns list of JSON string representation"""
+        if json_string is None:
+            return ([])
+        else:
+            return (json.loads(json_string))
+

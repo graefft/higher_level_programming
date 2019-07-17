980f052858be4e6c8223aae6454933ff2d3a4603
Thomas Graeff <676@holbertonschool.com>
2019-07-17 06:49:47 +0000

    Adds 12 13 14 15 16

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- Script that uses `hbtn_0d_tvshows` to list all genres of `Dexter`
+-- List genres
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'DEXTER'
+ORDER BY tv_genres.name ASC;
---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Script that lists all shows & all genres linked to that show from `hbtn_0d_tvshows`
+-- List shows and genres
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_genres.name ASC;

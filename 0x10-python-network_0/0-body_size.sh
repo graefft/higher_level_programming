3fb8a8c66898e147f6785807c9fac700e3552d29
graefft <676@holbertonschool.com>
2019-08-20 20:11:46 +0000

    Adds script to display size of body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Takes in URL, sends request to URL, displays size of body of response
-LENGTH=$(curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2)
-echo "$LENGTH"
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2

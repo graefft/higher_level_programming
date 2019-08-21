df24aeeb3eee2386b83ae2083f115d80446dbd75
Thomas Graeff <676@holbertonschool.com>
2019-08-21 06:29:39 +0000

    Adds Jason's algorithm to find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Finds a peak in list'''
+
+
+def find_peak(list_of_integers):
+    '''Finds peak in unsorted list'''
+    if not list_of_integers:
+        return None
+
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    if length == 2:
+        return max(list_of_integers)
+
+    mid = length // 2
+    lo = list_of_integers[mid - 1]
+    hi = list_of_integers[mid + 1]
+    peak = list_of_integers[mid]
+
+    if peak > lo and peak > hi:
+        return peak
+    elif peak < lo:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid:])

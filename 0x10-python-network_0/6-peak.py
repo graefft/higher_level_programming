9f08254bb68c984b9b9e190c291f9cad43df377e
Thomas Graeff <676@holbertonschool.com>
2019-08-21 22:58:06 +0000

    Adds comments to find_peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,20 +7,24 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
+    '''check ends'''
     length = len(list_of_integers)
     if length == 1:
         return list_of_integers[0]
     if length == 2:
         return max(list_of_integers)
 
-    mid = length // 2
+    '''compare 1 above & 1 below mid'''
     lo = list_of_integers[mid - 1]
     hi = list_of_integers[mid + 1]
     peak = list_of_integers[mid]
 
+    '''mid is a peak'''
     if peak > lo and peak > hi:
         return peak
+    '''lo is higher, make lo new mid'''
     elif peak < lo:
         return find_peak(list_of_integers[:mid])
+    '''hi is higher, make hi new mid'''
     else:
         return find_peak(list_of_integers[mid:])

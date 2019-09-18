c8c57cc02555a113cf5d4056f5901215ff3ba87c
Thomas Graeff <676@holbertonschool.com>
2019-09-18 01:45:12 +0000

    Simplifies 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,11 +3,7 @@
 const first = parseInt(process.argv[2]);
 const second = parseInt(process.argv[3]);
 
-if (isNaN(first) || isNaN(second)) {
-  console.log('NaN');
-} else {
-  console.log(add(first, second));
-}
+console.log(add(first, second));
 
 function add (a, b) {
   return a + b;
